ceb1999819039e8660a424d68326e71bf4f76432
Sebastian Santacruz <javier.santacruz@correounivalle.edu.co>
2026-01-13 16:20:47 -0500

    Primer Commit

---
+++ tintoLab/src/main/java/co/com/tintolab/TintoLabApplication.java
@@ -0,0 +1,13 @@
+package co.com.tintolab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TintoLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TintoLabApplication.class, args);
+	}
+
+}
